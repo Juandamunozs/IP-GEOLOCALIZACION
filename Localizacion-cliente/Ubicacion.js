cc34381fce77866c940c46eb0e1bd21f27c71c34
Juan David Muñoz Sotelo <juandavidmunozsotelo7@gmail.com>
2024-09-25 07:57:29 -0500

    Decir por que metodo se rastreo si IP o GPG

--- Localizacion-cliente/Ubicacion.js
+++ Localizacion-cliente/Ubicacion.js
@@ -46,11 +46,13 @@
 
 // VERSION ILIMITADA
 
+//Funcion similar al nGOnInit de Angular
 document.addEventListener('DOMContentLoaded', () => {
     obtenerLocalizacion();
-    setInterval(obtenerLocalizacion, 60000);
+    setInterval(obtenerLocalizacion, 6000);
 });
 
+//Funcion para obtener la localizacion por GPS
 function obtenerLocalizacion() {
     const tiempo = new Date();
     const hora = String(tiempo.getHours()).padStart(2, '0');
@@ -64,19 +66,25 @@ function obtenerLocalizacion() {
         buscarPorIP(hora, minutos, segundos);
     }
 
+    //Funcion para se se pudo obtener la ubicacion por GPS
     function success(position) {
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
         console.log("Ubicación obtenida por GPS, la latitud es " + lat + ", " + lon);
         buscarDatos(lat, lon, hora, minutos, segundos);
+        document.getElementById('ubicacion').textContent = 'Ubicación obtenida por GPS';
     }
 
+
+    //Funcion para se no se pudo obtener la ubicacion por GPS
     function error() {
         console.log("No se pudo obtener la ubicación GPS, buscando por IP...");
         buscarPorIP(hora, minutos, segundos);
+        document.getElementById('ubicacion').textContent = 'Ubicación obtenida por IP';
     }
 }
 
+//Funcion para buscar los datos de la ubicacion por GPS
 function buscarDatos(lat, lon, hora, minutos, segundos) {
     fetch(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`)
         .then(response => {
@@ -101,6 +109,7 @@ function buscarDatos(lat, lon, hora, minutos, segundos) {
         });
 }
 
+//Funcion para buscar los datos de la ubicacion por IP
 function buscarPorIP(hora, minutos, segundos) {
     fetch('https://ipapi.co/json/')
         .then(response => {
@@ -131,6 +140,7 @@ function buscarPorIP(hora, minutos, segundos) {
         });
 }
 
+//Funcion para mostrar la informacion de la ubicacion
 function mostrarInformacion(data, hora, minutos, segundos) {
     const locationInfo = `
         Horario: ${hora}:${minutos}:${segundos}<br>
@@ -140,6 +150,7 @@ function mostrarInformacion(data, hora, minutos, segundos) {
     document.getElementById('location').innerHTML = locationInfo;
 }
 
+//Funcion para mostrar la direccion de la ubicacion
 function mostrarDireccion(geocodeData) {
     const address = [
         geocodeData.address.house_number || 'N/A',
@@ -152,6 +163,7 @@ function mostrarDireccion(geocodeData) {
     document.getElementById('location').innerHTML += `<h3>Dirección:</h3> ${address}`;
 }
 
+//Funcion para mostrar el clima de la ubicacion
 function mostrarClima(weatherData) {
     const weatherInfo = `
         <h3>Información del Clima:</h3>
