7c0349651c4fa943af18fdd1d21b8119a5fbc84b
Juan David Muñoz Sotelo <162068766+Juandamunozs@users.noreply.github.com>
2024-09-25 12:18:10 -0500

    Update Ubicacion.js

--- Localizacion-cliente/Ubicacion.js
+++ Localizacion-cliente/Ubicacion.js
@@ -3,13 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
     setInterval(obtenerLocalizacion, 6000);
 });
 
+//Funcion para obtener la localizacion por GPS
 function obtenerLocalizacion() {
     const tiempo = new Date();
     const hora = String(tiempo.getHours()).padStart(2, '0');
     const minutos = String(tiempo.getMinutes()).padStart(2, '0');
     const segundos = String(tiempo.getSeconds()).padStart(2, '0');
-
-    //obtenerDispositivo();
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(success, error);
@@ -18,6 +17,7 @@ function obtenerLocalizacion() {
         buscarPorIP(hora, minutos, segundos);
     }
 
+    //Funcion para se se pudo obtener la ubicacion por GPS
     function success(position) {
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
@@ -26,6 +26,8 @@ function obtenerLocalizacion() {
         document.getElementById('ubicacion').textContent = 'Ubicación obtenida por GPS';
     }
 
+
+    //Funcion para se no se pudo obtener la ubicacion por GPS
     function error() {
         console.log("No se pudo obtener la ubicación GPS, buscando por IP...");
         buscarPorIP(hora, minutos, segundos);
@@ -33,15 +35,7 @@ function obtenerLocalizacion() {
     }
 }
 
-function obtenerDispositivo() {
-    const userAgent = navigator.userAgent;
-    console.log(userAgent);
-    
-    // Mostrar información del dispositivo
-    const deviceInfo = `Información del dispositivo: ${userAgent}`;
-    document.getElementById('location').innerHTML += `<h3>${deviceInfo}</h3>`;
-}
-
+//Funcion para buscar los datos de la ubicacion por GPS
 function buscarDatos(lat, lon, hora, minutos, segundos) {
     fetch(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`)
         .then(response => {
@@ -66,6 +60,7 @@ function buscarDatos(lat, lon, hora, minutos, segundos) {
         });
 }
 
+//Funcion para buscar los datos de la ubicacion por IP
 function buscarPorIP(hora, minutos, segundos) {
     fetch('https://ipapi.co/json/')
         .then(response => {
@@ -96,15 +91,17 @@ function buscarPorIP(hora, minutos, segundos) {
         });
 }
 
+//Funcion para mostrar la informacion de la ubicacion
 function mostrarInformacion(data, hora, minutos, segundos) {
     const locationInfo = `
         Horario: ${hora}:${minutos}:${segundos}<br>
         Latitud: ${data.latitude}<br>
         Longitud: ${data.longitude}<br>
     `;
-    document.getElementById('location').innerHTML += locationInfo;
+    document.getElementById('location').innerHTML = locationInfo;
 }
 
+//Funcion para mostrar la direccion de la ubicacion
 function mostrarDireccion(geocodeData) {
     const address = [
         geocodeData.address.house_number || 'N/A',
@@ -117,6 +114,7 @@ function mostrarDireccion(geocodeData) {
     document.getElementById('location').innerHTML += `<h3>Dirección:</h3> ${address}`;
 }
 
+//Funcion para mostrar el clima de la ubicacion
 function mostrarClima(weatherData) {
     const weatherInfo = `
         <h3>Información del Clima:</h3>
